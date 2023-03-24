0335584a11dd08ad4f79e9fabb659eb59f4ec001
risam <risam1984@gmail.com>
2023-03-24 22:02:21 +0700

    Done Tutorial 4 Circle Image

--- lib/widgets/sample_image_1.dart
+++ lib/widgets/sample_image_1.dart
@@ -12,11 +12,39 @@ class SimpleImage1 extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Image.asset('assets/images/sample-image.jpg'),
+          //Image.asset('assets/images/sample-image.jpg'),
           const CircleAvatar(
             radius: 50,
             backgroundImage: AssetImage('assets/images/sample-image.jpg'),
           ),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(50),
+            child: Image.asset(
+              'assets/images/sample-image.jpg',
+              height: 100,
+              width: 100,
+              fit: BoxFit.fill,
+            ),
+          ),
+          ClipOval(
+            child: Image.asset(
+              'assets/images/sample-image.jpg',
+              height: 100,
+              width: 100,
+              fit: BoxFit.fill,
+            ),
+          ),
+          Container(
+            height: 100,
+            width: 100,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                image: AssetImage('assets/images/sample-image.jpg'),
+                fit: BoxFit.fill
+              )
+            ),
+          )
         ],
       ),
     );
