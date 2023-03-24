3997a5e66015a6462b75a7958877068cd1f7d24d
risam <risam1984@gmail.com>
2023-03-24 23:06:06 +0700

    Done Tutorial ListTile

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.orange,
       ),
-      home: SampleListViewAvatar(),
+      home: SampleListView(),
     );
   }
 }
--- lib/widgets/sample_listview.dart
+++ lib/widgets/sample_listview.dart
@@ -4,22 +4,81 @@ import 'package:flutter/material.dart';
 class SampleListView extends StatelessWidget {
   SampleListView({super.key});
   final List colorCodes = [100, 200, 300, 400, 500, 600, 700, 800, 900];
-
+  final List data = [
+    {
+      'photoUrl': 'https://i.pravatar.cc/150?img=1',
+      'name': 'Saras',
+      'address': 'Banyumas',
+    },
+    {
+      'photoUrl': 'https://i.pravatar.cc/150?img=2',
+      'name': 'Tukiyem',
+      'address': 'Kebasen',
+    },
+    {
+      'photoUrl': 'https://i.pravatar.cc/150?img=3',
+      'name': 'Wartini',
+      'address': 'Cilongok',
+    },
+    {
+      'photoUrl': 'https://i.pravatar.cc/150?img=4',
+      'name': 'Markonah',
+      'address': 'Ajibarang',
+    },
+    {
+      'photoUrl': 'https://i.pravatar.cc/150?img=5',
+      'name': 'Slendro',
+      'address': 'Gumelar',
+    },
+    {
+      'photoUrl': 'https://i.pravatar.cc/150?img=6',
+      'name': 'Winarsih',
+      'address': 'Kebasen',
+    },
+    {
+      'photoUrl': 'https://i.pravatar.cc/150?img=7',
+      'name': 'Warko',
+      'address': 'Karang Rau',
+    },
+    {
+      'photoUrl': 'https://i.pravatar.cc/150?img=8',
+      'name': 'Ponijah',
+      'address': 'Banyumas',
+    },
+    {
+      'photoUrl': 'https://i.pravatar.cc/150?img=9',
+      'name': 'Sukir',
+      'address': 'Karanglewas',
+    },
+  ];
   @override
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Latihan ListView'),
+        title: const Text('Latihan Widget ListTile'),
       ),
-      body: ListView.builder(
+      body: ListView.separated(
         padding: const EdgeInsets.all(10),
         itemBuilder: (context, index){
-          return Container(
-            color: Colors.teal[colorCodes[index]],
-            height: 100,
+          return ListTile(
+            leading: Image.network(data[index]['photoUrl']),
+            title: Text(data[index]['name']),
+            subtitle: Text(data[index]['address']),
+            trailing: IconButton(
+              icon: const Icon(Icons.more_vert),
+              onPressed: (){},
+            ),
+            tileColor: Colors.grey[400],
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
           );
         },
-        itemCount: colorCodes.length,
+        itemCount: data.length,
+        separatorBuilder: (context, index){
+          return const Divider(
+            thickness: 2,
+            color: Colors.grey,
+          );
+        },
       )
     );
   }
